f287200a7c8a9c934c7b320ddf0f414f29c36572
Niran Wangyad <niran.w@cicnetgrp.net>
2024-11-28 10:09:01 +0700

    update

--- console/migrations/m240501_152000_create_customer_table.php
+++ console/migrations/m240501_152000_create_customer_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%customer}}`.
  */
-class m241119_080511_create_customer_table extends Migration
+class m240501_152000_create_customer_table extends Migration
 {
     /**
      * {@inheritdoc}

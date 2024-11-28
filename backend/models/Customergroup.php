bf7e1943d70f997175bbdfdd521e39a9bcc9a021
Niran Wangyad <niran.w@cicnetgrp.net>
2024-11-28 10:13:57 +0700

    update

--- backend/models/Customergroup.php
+++ backend/models/Customergroup.php
@@ -3,6 +3,7 @@
 namespace backend\models;
 
 use Yii;
+use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "customergroup".
@@ -19,5 +20,58 @@ use Yii;
  */
 class Customergroup extends \common\models\CustomerGroup
 {
-
+    public function behaviors()
+    {
+        return [
+            'timestampcdate' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'created_at',
+                ],
+                'value' => time(),
+            ],
+            'timestampudate' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'updated_at',
+                ],
+                'value' => time(),
+            ],
+            'timestampcby' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'created_by',
+                ],
+                'value' => Yii::$app->user->id,
+            ],
+            'timestamuby' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_by',
+                ],
+                'value' => Yii::$app->user->id,
+            ],
+//            'timestampcompany'=>[
+//                'class'=> \yii\behaviors\AttributeBehavior::className(),
+//                'attributes'=>[
+//                    ActiveRecord::EVENT_BEFORE_INSERT=>'company_id',
+//                ],
+//                'value'=> isset($_SESSION['user_company_id'])? $_SESSION['user_company_id']:1,
+//            ],
+//            'timestampbranch'=>[
+//                'class'=> \yii\behaviors\AttributeBehavior::className(),
+//                'attributes'=>[
+//                    ActiveRecord::EVENT_BEFORE_INSERT=>'branch_id',
+//                ],
+//                'value'=> isset($_SESSION['user_branch_id'])? $_SESSION['user_branch_id']:1,
+//            ],
+            'timestampupdate' => [
+                'class' => \yii\behaviors\AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_at',
+                ],
+                'value' => time(),
+            ],
+        ];
+    }
 }
